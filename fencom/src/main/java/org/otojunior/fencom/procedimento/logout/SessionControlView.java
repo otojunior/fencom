f90128f6c1153d10a2b59435f295de036a8b954e
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-06-28 10:32:25 -0300

    Login com cooperativa embutido

--- fencom/src/main/java/org/otojunior/fencom/procedimento/logout/SessionControlView.java
+++ fencom/src/main/java/org/otojunior/fencom/procedimento/logout/SessionControlView.java
@@ -3,6 +3,9 @@
  */
 package org.otojunior.fencom.procedimento.logout;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -20,6 +23,8 @@ public class SessionControlView {
 	
 	@Inject
 	private InitService service;
+	
+	private Cooperativa cooperativa;
 	
 	/**
 	 * Logout
@@ -40,5 +45,68 @@ public class SessionControlView {
 	public String logado() {
 		service.getUsuarioLogado();
 		return null;
+	}
+	
+	/**
+	 * 	
+	 * @return
+	 */
+	public List<Cooperativa> getCooperativas() {
+		/*
+		 *	SIMULAÇÃO DE PESQUISA EM BANCO 
+		 */
+		return Arrays.asList(
+			new Cooperativa(1L, "Cooperativa 1"),
+			new Cooperativa(2L, "Cooperativa 2"),
+			new Cooperativa(3L, "Cooperativa 3")
+		);
+	}
+	
+	
+	/**
+	 * Classe estática para simular pesquisa em tabela.
+	 * @author 01456231650
+	 */
+	/*
+	 *	SIMULAÇÃO 
+	 */
+	public static class Cooperativa {
+		private Long id;
+		private String nome;
+		
+		/**
+		 * 
+		 * @param id
+		 * @param nome
+		 */
+		public Cooperativa(Long id, String nome) {
+			this.id = id;
+			this.nome = nome;
+		}
+		
+		/**
+		 * @return the id
+		 */
+		public Long getId() { return id; }
+		
+		/**
+		 * @return the nome
+		 */
+		public String getNome() { return nome; }
+	}
+
+
+	/**
+	 * @return the cooperativa
+	 */
+	public Cooperativa getCooperativa() {
+		return cooperativa;
+	}
+
+	/**
+	 * @param cooperativa the cooperativa to set
+	 */
+	public void setCooperativa(Cooperativa cooperativa) {
+		this.cooperativa = cooperativa;
 	}
 }
